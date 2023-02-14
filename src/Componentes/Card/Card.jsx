6a8c9f1fe3ea5c1147c4f79c6e66e9949cbb67d8
Michael Luis Condori Paye <luis2003nb@gmail.com>
2023-02-13 23:07:49 -0300

    Tengo 24 hs First commit

---
+++ src/Componentes/Card/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+ const Card = ()=> {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+  
+
+  export default Card;
---
+++ src/Componentes/Cards/Cards.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+
+ const List = ()=> {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+  
+
+  export default List;
